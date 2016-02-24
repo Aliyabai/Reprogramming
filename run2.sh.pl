1827dbcfd19c8749352a750751f9fcfc525ddc66
Aliya <1326992019@qq.com>
2016-02-24 10:32:38 +0800

    Create run2.sh.pl
    由pipeup文件建stat文件

---
+++ run2.sh.pl
@@ -0,0 +1,17 @@
+#!usr/bin/perl -w
+use strict;
+die "USAGE: perl $0 srrlist" if @ARGV < 1;
+my $pileupStat = "0.bin/pileup.stat.pl";
+my $dir = "/ifs5/ST_COMG/USER/baiyali/RNA_editing/Reprogramming/data_process/new.data.work/ADAR_motif";
+
+open IN, "$ARGV[0]" or die $!;
+open OUT, ">run2.sh" or die $!;
+while(<IN>){
+    chomp;
+    my $srr = (split /\t/,)[0];
+    print OUT "perl $pileupStat $dir/pipeup/$srr.pipeup > $dir/Stat/$srr.pipeup.stat\n";
+}
+close OUT;
+
+###perl 0.bin/pileup.stat.pl pipeup/SRR1107924.pipeup > Stat/SRR1107924.pipeup.stat
+###建STAT文件
