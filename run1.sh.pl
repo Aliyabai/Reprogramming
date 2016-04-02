cc070433599e82738af70a4c993389f5e58025c5
Aliya <1326992019@qq.com>
2016-04-02 17:17:16 +0800

    Update run1.sh.pl
    second step:
    build pipeup

--- run1.sh.pl
+++ run1.sh.pl
@@ -14,3 +14,5 @@ while(<IN>){
     print OUT "/usr/bin/perl $pileupSite --bam $bam --reg $reg --out /ifs5/ST_COMG/USER/baiyali/RNA_editing/Reprogramming/data_process/new.data.work/ADAR_motif/pipeup/$srr.pipeup --ref $ref --minMQ 20 --minBQ 20 --uniq\n";
 }
 close OUT;
+
+#/usr/bin/perl /ifs1/ST_SYSBIO/USER/liudb2/RNA_editing/bladder/xiongheng/bin/pileupSite.pl --bam /ifs5/ST_COMG/USER/baiyali/RNA_editing/Reprogramming/data_process/SRR1107924/new/bamprocess/SRR1107924.reorder.markdup.baseRecal.bam --reg position/New.filter.pos.reg --out pipeup/SRR1107924.new.pipeup --ref /ifs1/ST_SYSBIO/USER/liudb2/database/reference/hg19/hg19.fa --minMQ 20 --minBQ 20 --uniq
